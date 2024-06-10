f97e8a32c7bd432a0e8463c0ee117afceb4bb2f8
Atieng <atiengivylisa@gmail.com>
2024-06-09 20:44:56 -0700

    update .py file

--- customer_churn_analysis.py
+++ customer_churn_analysis.py
@@ -1,27 +1,3 @@
-# import pandas as pd
-
-# class CustomerChurnDataset:
-    
-#     def __init__(self, file_path):
-#         self.df = pd.read_csv(file_path)
-
-#     def df_info(self):
-#         return self.df.info()
-        
-#     def statistical_summary(self):
-#         return self.df.describe()
-
-#     def get_columns(self):
-#         return self.df.columns
-
-#     def get_dtypes(self):
-#         return self.df.dtypes
-
-#     def df_shape(self):
-#         return self.df.shape
-
-#     def get_head(self, n=5):  
-#         return self.df.head(n)
 import pandas as pd
 
 class CustomerChurnDataset:
@@ -55,7 +31,3 @@ class CustomerChurnDataset:
 
  
 
- 
-         
-
- 
